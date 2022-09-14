f648f49834305edf1564fc35ea4a7600f3f31fc2
Antonio Victor <Antoniovictor12@live.com>
2022-09-13 21:31:15 -0300

    layout inicial mobile pronto

---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { View, Image, FlatList } from "react-native";
+import LogoImg from "../../assets/logo-nlw-esports.png";
+import { GameCard } from "../../components/GameCard";
+import { Heading } from "../../components/Heading";
+import { GAMES } from "../../utils/games";
+
+import { styles } from "./styles";
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image source={LogoImg} style={styles.logo} />
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+      <FlatList
+        contentContainerStyle={styles.contentList}
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => <GameCard data={item} />}
+      />
+    </View>
+  );
+}
